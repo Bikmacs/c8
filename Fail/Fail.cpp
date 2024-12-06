5a4e4d25b82b6df1ae9f7766352902978a8a1a0b
Bikmacs <macsim.shmelev@yandex.ru>
2024-12-07 00:18:16 +0500

    master

---
+++ Fail/Fail.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cstdio>
+
+int main() {
+    FILE* file;
+    if (fopen_s(&file, "1.txt", "r") != 0) {
+        std::cout << "Файл невозможно открыть или создать" << std::endl;
+        return 1; 
+    }
+
+    double a, b;
+    while (fscanf_s(file, "%lf %lf", &a, &b)==2) {
+        printf("y = %.2lfx + %.2lf\n", a, b);
+    }
+
+    fclose(file);
+    return 0;
+}
