dc5eb2e2664864fb4aaa1b286ade5969dd79344a
Bikmacs <macsim.shmelev@yandex.ru>
2025-01-18 10:44:15 +0500

    m

--- Fail/Fail.cpp
+++ Fail/Fail.cpp
@@ -1,18 +1,42 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include <cstdio>
 
-int main() {
-    FILE* file;
-    if (fopen_s(&file, "1.txt", "r") != 0) {
-        std::cout << "Файл невозможно открыть или создать" << std::endl;
-        return 1; 
-    }
+void Vvod(double& a, double& b) {
+    std::cout << "Введите a: ";
+    std::cin >> a;
+    std::cout << "Введите b: ";
+    std::cin >> b;
+}
 
-    double a, b;
-    while (fscanf_s(file, "%lf %lf", &a, &b)==2) {
-        printf("y = %.2lfx + %.2lf\n", a, b);
+int main() {
+    setlocale(LC_ALL, "rus");
+    FILE* file;
+    if ((file = fopen("Rezultat.txt", "wx")) == NULL) {
+        std::cout << "Файл невозможно открыть или создать" << std::endl;
+        return 1;
+    }
+    double a, b, coor, x = 0;
+
+    while (true) {
+        Vvod(a, b);
+        if (a < 0 || b < 0) {
+            std::cout << "числа отрицательные, пока";
+            return 1;
+        }
+        else
+        {
+            if (a == 0 && b == 0) {
+                break;
+            }
+            coor = (a * x) + b; 
+            printf("y = %.2lfx + %.2lf (%2.lf,%2.lf )\n", a, b, x, coor);
+            fprintf(file, "y = %.2lfx + %.2lf (%2.lf,%2.lf )\n", a, b, x, coor);
+            
+        }
     }
 
     fclose(file);
+    std::cout << "Данные записаны в файл Rezultat.txt" << std::endl;
     return 0;
 }
